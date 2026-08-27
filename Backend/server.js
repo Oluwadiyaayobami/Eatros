9d830183dda120541eff9218e650cb06eb88b93e
oluwadiyaayobami <eniolaabimbola09@gmail.com>
2026-08-27 14:13:30 +0100

    cors

--- Backend/server.js
+++ Backend/server.js
@@ -33,7 +33,7 @@ const port = process.env.PORT || 5000
 
 
 app.use(cors({
-  origin: ["https://paycoreng.vercel.app", "https://paycore-ng.vercel.app", "http://localhost:3000"],
+  origin: ["https://eatros.vercel.app", "http://localhost:3000"],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
